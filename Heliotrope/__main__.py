545b1347a145f1dec4d6ad54072aaa2cd2d1c50a
SaidBySolo <saidbysolo@gmail.com>
2020-09-04 16:35:35 +0900

    feat(utils): index, hitomi info endpoint

---
+++ Heliotrope/__main__.py
@@ -0,0 +1,3 @@
+from Heliotrope.app import *
+
+app.run(host="0.0.0.0", port=8000)
---
+++ Heliotrope/app.py
@@ -0,0 +1,31 @@
+from sanic import Sanic
+from sanic.response import json
+from sanic.exceptions import abort
+
+from Heliotrope.utils.hitomi import hitomi
+
+app = Sanic(__name__)
+
+
+@app.route("/api/hitomi/<index>")
+async def info(request, index: int):
+    json_ = await hitomi.info(index)
+    if not json_:
+        return abort(404)
+    else:
+        return json(json_)
+
+
+@app.route("/api/hitomi/galleryinfo/<index>")
+async def galleryinfo(request, index: int):
+    json_ = await hitomi.galleryinfo(index)
+    if not json_:
+        return abort(404)
+    else:
+        return json(json_)
+
+
+@app.route("/api/hitomi/integrated/<index>")
+async def inte_info(request, index: int):
+    json_ = await hitomi.integrated_info(index)
+    return json(json_)
