9955665c5df4b0f2fe7176f609afdb2288bb8ae4
Restyled.io <commits@restyled.io>
2020-09-04 07:48:58 +0000

    Restyled by black

--- Heliotrope/utils/hitomi/hitomi.py
+++ Heliotrope/utils/hitomi/hitomi.py
@@ -93,14 +93,7 @@ async def integrated_info(index: int):
             "tags": tags.tags,
         }
 
-    data = {
-        "data": [
-            {
-                "galleryinfo": gi,
-                "tags": ts,
-            }
-        ]
-    }
+    data = {"data": [{"galleryinfo": gi, "tags": ts,}]}
 
     return data
 
