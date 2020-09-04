7804fa253c1bd8373718c1beb6572245f9705f4b
Restyled.io <commits@restyled.io>
2020-09-04 17:32:40 +0000

    Restyled by black

--- test/test_unexpected_response.py
+++ test/test_unexpected_response.py
@@ -1,12 +1,15 @@
 from Heliotrope.app import app
 
+
 def test_index_unexpected_response():
     request, response = app.test_client.get("/api/hitomi/1111111111111111")
     assert response.status == 404
 
+
 def test_galleryindo_unexpected_response():
     request, response = app.test_client.get("/api/hitomi/galleryinfo/1111111111111111")
     assert response.status == 404
+
 
 def test_list_unexpected_response():
     request, response = app.test_client.get("/api/hitomi/list/1111111111111111")
