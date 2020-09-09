45939279b1e444f9dcf48d6fb249a61c8d99921f
SaidBySolo <saidbysolo@gmail.com>
2020-09-09 16:01:08 +0900

    test(test): add headers

--- test/test_response.py
+++ test/test_response.py
@@ -1,21 +1,28 @@
+import os
 from Heliotrope.app import app
+
+headers = {"Authorization": os.environ["Authorization"]}
 
 
 def test_info_response():
-    request, response = app.test_client.get("/api/hitomi/info/1496588")
+    request, response = app.test_client.get("/api/hitomi/info/1496588", headers=headers)
     assert response.status == 200
 
 
 def test_galleryindo_response():
-    request, response = app.test_client.get("/api/hitomi/galleryinfo/1496588")
+    request, response = app.test_client.get(
+        "/api/hitomi/galleryinfo/1496588", headers=headers
+    )
     assert response.status == 200
 
 
 def test_integrated_info_response():
-    request, response = app.test_client.get("/api/hitomi/integrated/1496588")
+    request, response = app.test_client.get(
+        "/api/hitomi/integrated/1496588", headers=headers
+    )
     assert response.status == 200
 
 
 def test_list_response():
-    request, response = app.test_client.get("/api/hitomi/list/1")
+    request, response = app.test_client.get("/api/hitomi/list/1", headers=headers)
     assert response.status == 200
