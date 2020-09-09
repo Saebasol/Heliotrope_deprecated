2be1769fefbef6512d5265e6797599f7ad0104b5
SaidBySolo <saidbysolo@gmail.com>
2020-09-09 15:53:31 +0900

    feat(api): add authorized decorator

--- Heliotrope/api/hitomi/galleryinfo.py
+++ Heliotrope/api/hitomi/galleryinfo.py
@@ -2,11 +2,13 @@ from sanic import Blueprint
 from Heliotrope.utils.hitomi import hitomi
 from sanic.exceptions import abort
 from sanic.response import json
+from Heliotrope.utils.checker.check import authorized
 
 galleryinfo = Blueprint("hitomi_galleryinfo", url_prefix="/galleryinfo")
 
 
 @galleryinfo.route("/<index>")
+@authorized()
 async def hitomi_galleryinfo(request, index: int):
     json_ = await hitomi.galleryinfo(index)
     if not json_:
--- Heliotrope/api/hitomi/integrated.py
+++ Heliotrope/api/hitomi/integrated.py
@@ -1,11 +1,13 @@
 from sanic import Blueprint
-from Heliotrope.utils.hitomi import hitomi
 from sanic.response import json
+from Heliotrope.utils.hitomi import hitomi
+from Heliotrope.utils.checker.check import authorized
 
 integrated = Blueprint("hitomi_integrated", url_prefix="/integrated")
 
 
 @integrated.route("/<index>")
+@authorized()
 async def hitomi_integrated(request, index: int):
     json_ = await hitomi.integrated_info(index)
     return json(json_)
