0c23091fab25de4865aee7e4e56051c3bd5a039f
SaidBySolo <saidbysolo@gmail.com>
2020-09-09 15:33:28 +0900

    feat(utils): add check authorized

---
+++ Heliotrope/utils/checker/check.py
@@ -0,0 +1,32 @@
+from functools import wraps
+from sanic.response import json
+
+
+def check_request_for_authorization_status(request):
+    token = request.headers.get("headers")
+    if not token["Authorization"] or token["Authorization"] not in "SaidBySolo:)":
+        return False
+    else:
+        return True
+
+
+def authorized():
+    def decorator(f):
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+            # run some method that checks the request
+            # for the client's authorization status
+            is_authorized = check_request_for_authorization_status(request)
+
+            if is_authorized:
+                # the user is authorized.
+                # run the handler method and return the response
+                response = await f(request, *args, **kwargs)
+                return response
+            else:
+                # the user is not authorized.
+                return json({"status": "not_authorized"}, 403)
+
+        return decorated_function
+
+    return decorator
