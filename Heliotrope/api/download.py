a7c1db12240b3e83ddc7548c6277f7bd2ef3e8f5
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-10 19:18:55 +0900

    refactor(utils): check folder
    teat(test): add download api test code
    refactor(api): change check folder

--- Heliotrope/api/download.py
+++ Heliotrope/api/download.py
@@ -1,5 +1,5 @@
 from Heliotrope.utils.checker.check import authorized
-from Heliotrope.utils.downloader.download import check_vaild, compression_or_download
+from Heliotrope.utils.downloader.download import check_folder_or_download
 from sanic import Blueprint
 from sanic.response import json
 from Heliotrope.utils.hitomi import hitomi
@@ -13,8 +13,11 @@ download = Blueprint("image_download", url_prefix="/download")
 )
 @authorized()
 async def downloader(request):
-    download_bool = request.json.get("download")
     index = request.json.get("index")
+    download_bool = request.json.get("download")
     if not download_bool or not index:
         return json({"status": "bad_request"}, 400)
-    
+
+    result = check_folder_or_download(index, download_bool)
+
+    return result
