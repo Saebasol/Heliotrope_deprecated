52ba8daf121fccc4c4d8d5044b846b01ff9ab495
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-10 22:34:32 +0900

    test(test): refactor use pytest-sanic

--- test/test_unexpected_response.py
+++ test/test_unexpected_response.py
@@ -1,25 +1,21 @@
 import os
-from Heliotrope.app import app
+
 
 headers = {"Authorization": os.environ["Authorization"]}
 
 
-def test_info_unexpected_response():
-    request, response = app.test_client.get(
-        "/api/hitomi/info/1111111111111111", headers=headers
-    )
+async def test_info_unexpected_response(test_cli):
+    response = await test_cli.get("/api/hitomi/info/1111111111111111", headers=headers)
     assert response.status == 404
 
 
-def test_galleryindo_unexpected_response():
-    request, response = app.test_client.get(
+async def test_galleryindo_unexpected_response(test_cli):
+    response = await test_cli.get(
         "/api/hitomi/galleryinfo/1111111111111111", headers=headers
     )
     assert response.status == 404
 
 
-def test_list_unexpected_response():
-    request, response = app.test_client.get(
-        "/api/hitomi/list/1111111111111111", headers=headers
-    )
+async def test_list_unexpected_response(test_cli):
+    response = await test_cli.get("/api/hitomi/list/1111111111111111", headers=headers)
     assert response.status == 404
