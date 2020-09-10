e8956089e46552499184d9c86e4c584b0e7c056a
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-10 22:34:32 +0900

    test(test): refactor use pytest-sanic

--- test/test_response.py
+++ test/test_response.py
@@ -1,74 +1,67 @@
+import asyncio
 import os
-from Heliotrope.app import app
-import time
 
 headers = {"Authorization": os.environ["Authorization"]}
 
 
-def test_info_response():
-    request, response = app.test_client.get("/api/hitomi/info/1496588", headers=headers)
+async def test_info_response(test_cli):
+    response = await test_cli.get("/api/hitomi/info/1496588", headers=headers)
     assert response.status == 200
 
 
-def test_galleryindo_response():
-    request, response = app.test_client.get(
-        "/api/hitomi/galleryinfo/1496588", headers=headers
-    )
+async def test_galleryindo_response(test_cli):
+    response = await test_cli.get("/api/hitomi/galleryinfo/1496588", headers=headers)
     assert response.status == 200
 
 
-def test_integrated_info_response():
-    request, response = app.test_client.get(
-        "/api/hitomi/integrated/1496588", headers=headers
-    )
+async def test_integrated_info_response(test_cli):
+    response = await test_cli.get("/api/hitomi/integrated/1496588", headers=headers)
     assert response.status == 200
 
 
-def test_list_response():
-    request, response = app.test_client.get("/api/hitomi/list/1", headers=headers)
+async def test_list_response(test_cli):
+    response = await test_cli.get("/api/hitomi/list/1", headers=headers)
     assert response.status == 200
 
 
-def test_download_response():
-    request, response = app.test_client.post(
+async def test_download_response(test_cli):
+    response = await test_cli.post(
         "/api/download", headers=headers, json={"download": False, "index": 1}
     )
-    assert (
-        response.status == 200
-        and response.json["status"] == "pending"
-        and response.json["total"] == 2
-    )
+    await asyncio.sleep(5)
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json == {"status": "pending", "total": 2}
 
 
-def test_download_response_already():
-    time.sleep(10)
-    request, response = app.test_client.post(
+async def test_download_response_already(test_cli):
+    response = await test_cli.post(
         "/api/download", headers=headers, json={"download": False, "index": 1}
     )
-    assert (
-        response.status == 200
-        and response.json["status"] == "already"
-        and response.json["total"] == 2
-    )
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json == {"status": "already", "total": 2}
 
 
-def test_download_zip_response():
-    request, response = app.test_client.post(
+async def test_download_zip_response(test_cli):
+    response = await test_cli.post(
         "/api/download", headers=headers, json={"download": True, "index": 1}
     )
-    assert (
-        response.status == 200
-        and response.json["status"] == "use_cached"
-        and response.json["link"] == "https://doujinshiman.ga/download/1/1.zip"
-    )
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json == {
+        "status": "use_cached",
+        "link": "https://doujinshiman.ga/download/1/1.zip",
+    }
 
 
-def test_download_zip_response_already():
-    request, response = app.test_client.post(
+async def test_download_zip_response_already(test_cli):
+    response = await test_cli.post(
         "/api/download", headers=headers, json={"download": True, "index": 1}
     )
-    assert (
-        response.status == 200
-        and response.json["status"] == "already"
-        and response.json["link"] == "https://doujinshiman.ga/download/1/1.zip"
-    )
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json == {
+        "status": "already",
+        "link": "https://doujinshiman.ga/download/1/1.zip",
+    }
