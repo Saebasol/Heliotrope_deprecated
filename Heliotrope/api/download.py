acedd590d7c79b83ea9ef4a66522635d1e48d847
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-10 19:47:32 +0900

    fix(api): check type

--- Heliotrope/api/download.py
+++ Heliotrope/api/download.py
@@ -2,7 +2,6 @@ from Heliotrope.utils.checker.check import authorized
 from Heliotrope.utils.downloader.download import check_folder_or_download
 from sanic import Blueprint
 from sanic.response import json
-from Heliotrope.utils.hitomi import hitomi
 
 download = Blueprint("image_download", url_prefix="/download")
 
@@ -15,7 +14,7 @@ download = Blueprint("image_download", url_prefix="/download")
 async def downloader(request):
     index = request.json.get("index")
     download_bool = request.json.get("download")
-    if download_bool is not None or not index:
+    if download_bool is None or not index:
         return json({"status": "bad_request"}, 400)
 
     result = await check_folder_or_download(index, download_bool)
