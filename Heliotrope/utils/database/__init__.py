ca40d688e4a2042a2b3a70af0485544f693f9c11
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-13 20:52:22 +0900

    refactor(utils): will use sanic integration

--- Heliotrope/utils/database/__init__.py
+++ Heliotrope/utils/database/__init__.py
@@ -1,21 +1,4 @@
-import os
-
-from tortoise import Tortoise
 from .models import User
 from .user_management import check_user, user_register
-
-
-async def init():
-    await Tortoise.init(
-        db_url=f"mysql://{os.environ['DB_UNAME']}:{os.environ['DB_PW']}@{os.environ['DB_HOST']}:3306/{os.environ['DB_DBNAME']}",
-        modules={"models": ["Heliotrope.utils.database.models"]},
-    )
-
-    await Tortoise.generate_schemas(safe=True)
-
-
-async def close():
-    await Tortoise.close_connections()
-
 
 __all__ = ["User", "check_user", "user_register"]
