c7764e8fdb9327acbafc3651e65bb41cd71ce903
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-13 19:32:41 +0900

    refactor(utils): use fullpath

--- Heliotrope/utils/database/__init__.py
+++ Heliotrope/utils/database/__init__.py
@@ -8,7 +8,7 @@ from .user_management import check_user, user_register
 async def init():
     await Tortoise.init(
         db_url=f"mysql://{os.environ['DB_UNAME']}:{os.environ['DB_PW']}@{os.environ['DB_HOST']}:3306/{os.environ['DB_DBNAME']}",
-        modules={"models": ["database.models"]},
+        modules={"models": ["Heliotrope.utils.database.models"]},
     )
 
     await Tortoise.generate_schemas(safe=True)
