a6abb9fd9e1f0d3dc4d977e9f19d2ba3875430a8
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-13 20:53:53 +0900

    refactor(utils): moved in download

--- Heliotrope/utils/database/user_management.py
+++ Heliotrope/utils/database/user_management.py
@@ -2,19 +2,6 @@ from sanic.response import json
 from Heliotrope.utils.database import User
 
 
-async def check_user(user_id):
-    user_data = await User.get_or_none(user_id=user_id)
-    if not user_data:
-        return json({"status": "need_register"}, 403)
-    else:
-        count = user_data.download_count
-        if count >= 5:
-            return json({"status": "Too_many_requests"}, 429)
-        else:
-            user_data.download_count = count + 1
-            await user_data.save()
-
-
 async def user_register(user_id):
     user_data = await User.get_or_none(user_id=user_id)
     if user_data:
