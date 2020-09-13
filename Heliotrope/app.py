1fe80b402a8fa2886bd3ab35784a20eda5bdf275
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-13 14:14:40 +0900

    feat(Heliotrope): add database init and close

--- Heliotrope/app.py
+++ Heliotrope/app.py
@@ -5,10 +5,22 @@ from sanic import Sanic
 from sentry_sdk.integrations.sanic import SanicIntegration
 
 from Heliotrope.api import api
+from Heliotrope.utils import database
 
 sentry_sdk.init(
-    dsn=os.environ["sentry"], integrations=[SanicIntegration()],
+    dsn=os.environ["sentry"],
+    integrations=[SanicIntegration()],
 )
 
 app = Sanic(__name__)
 app.blueprint(api)
+
+
+@app.listener("before_server_start")
+async def setup_db(app, loop):
+    await database.init()
+
+
+@app.listener("after_server_stop")
+async def close_db(app, loop):
+    await database.close()
