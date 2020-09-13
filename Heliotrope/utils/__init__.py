f8871df8d9080f6e96199e29411cc7a8006e7584
Restyled.io <commits@restyled.io>
2020-09-13 13:03:40 +0000

    style(isort): apply code style

--- Heliotrope/utils/__init__.py
+++ Heliotrope/utils/__init__.py
@@ -1,7 +1,4 @@
-from . import hitomi
-from . import checker
-from . import database
-from . import downloader
+from . import checker, database, downloader, hitomi
 from .option import config
 
 __all__ = ["hitomi", "checker", "database", "downloader", "config"]
