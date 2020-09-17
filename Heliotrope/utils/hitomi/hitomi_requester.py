20875f1a91bf1f752d109353421a3e96f4f90a48
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-17 18:43:08 +0900

    fix(hitomi): f-string backslash

--- Heliotrope/utils/hitomi/hitomi_requester.py
+++ Heliotrope/utils/hitomi/hitomi_requester.py
@@ -26,7 +26,10 @@ async def get_galleryinfo(index: int):
 
 
 async def get_gallery(galleryinfomodel: HitomiGalleryInfoModel):
-    url = f"https://hitomi.la/{galleryinfomodel.type_ if galleryinfomodel.type_.lower() != 'artistcg' else 'cg'}/{re.sub(r'([\"|\'|\%|\(|\)|\{|\}|\[|\]|\<|\>])', '-', galleryinfomodel.title).replace(' ', '-')}-{galleryinfomodel.language_localname}-{galleryinfomodel.galleryid}.html".lower()
+    url_title = re.sub(
+        r"([\"|\'|\%|\(|\)|\{|\}|\[|\]|\<|\>])", "-", galleryinfomodel.title
+    ).replace(" ", "-")
+    url = f"https://hitomi.la/{galleryinfomodel.type_ if galleryinfomodel.type_.lower() != 'artistcg' else 'cg'}/{url_title}-{galleryinfomodel.language_localname}-{galleryinfomodel.galleryid}.html".lower()
     async with aiohttp.ClientSession() as cs:
         async with cs.get(url, headers=headers) as r:
             if r.status != 200:
