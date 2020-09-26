8c1b83dd0fd7a68fafcb8c2fab5dbfc2629951c5
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-26 18:13:15 +0900

    fix(utils): handle case

--- Heliotrope/image_proxy.py
+++ Heliotrope/image_proxy.py
@@ -1,5 +1,6 @@
 from sanic import Blueprint
-from sanic.response import response
+from sanic import response
+from sanic.response import json
 
 from Heliotrope.utils.downloader.download import thumbnail_cache
 
@@ -12,5 +13,8 @@ proxy = Blueprint("image_proxy", url_prefix="/proxy")
     methods=["GET"],
 )
 async def image_proxy(request, path: str):
-    await thumbnail_cache(path)
-    return await response.file(f"/var/www{path}")
+    cached = await thumbnail_cache(path)
+    if cached:
+        return await response.file(f"/var/www/proxy{path}")
+    else:
+        return json({"status": "not_found"}, 404)
