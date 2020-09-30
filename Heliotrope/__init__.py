9a4f6e7d443ac57eb18af71d9999e431429dd01c
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 22:58:32 +0900

    style(Heliotrope): version info update

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -4,4 +4,4 @@ __version__ = "1.5.0"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=1, minor=1, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel="final", serial=0)
