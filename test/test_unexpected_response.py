108ec5d2c71c7ed950d78a8b78c7a3f5226e7a1e
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 19:26:33 +0900

    test(test): apply v1, and progress test

--- test/test_unexpected_response.py
+++ test/test_unexpected_response.py
@@ -4,22 +4,22 @@ headers = {"Authorization": os.environ["Authorization"]}
 
 
 async def test_info_unexpected_response(test_cli):
-    response = await test_cli.get("/api/hitomi/info/1111111111111111", headers=headers)
+    response = await test_cli.get("/v1/api/hitomi/info/1111111111111111", headers=headers)
     assert response.status == 404
 
 
 async def test_galleryindo_unexpected_response(test_cli):
     response = await test_cli.get(
-        "/api/hitomi/galleryinfo/1111111111111111", headers=headers
+        "/v1/api/hitomi/galleryinfo/1111111111111111", headers=headers
     )
     assert response.status == 404
 
 
 async def test_list_unexpected_response(test_cli):
-    response = await test_cli.get("/api/hitomi/list/1111111111111111", headers=headers)
+    response = await test_cli.get("/v1/api/hitomi/list/1111111111111111", headers=headers)
     assert response.status == 404
 
 
 async def test_thumbnail_unexpected_response(test_cli):
-    response = await test_cli.get("/proxy/balbalblabla")
+    response = await test_cli.get("/v1/api/proxy/balbalblabla")
     assert response.status == 404
