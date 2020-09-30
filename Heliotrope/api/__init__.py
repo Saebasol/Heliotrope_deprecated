5650407b53f1608bad1891c88626ffe37f8a3603
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 16:38:02 +0900

    style(api): move image_proxy

--- Heliotrope/api/__init__.py
+++ Heliotrope/api/__init__.py
@@ -3,5 +3,6 @@ from sanic import Blueprint
 from .download import download
 from .hitomi import hitomi
 from .register import register
+from .image_proxy import proxy
 
-api = Blueprint.group(hitomi, download, register, url_prefix="/api")
+api = Blueprint.group(hitomi, download, register, proxy, url_prefix="/api")
