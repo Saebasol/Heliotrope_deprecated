eb12361a0e626903f1d603001de00fb1a2d83651
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 22:26:54 +0900

    fix(task_progress): append -> extend

--- Heliotrope/utils/downloader/task_progress.py
+++ Heliotrope/utils/downloader/task_progress.py
@@ -10,7 +10,7 @@ class TaskProgress:
         task_dict_list = [{"task_name": task.get_name(), "count": count, "task": task}]
         if await self.cache.exists(user_id):
             already_in_list = await self.cache.get(user_id)
-            already_in_list.append(task_dict_list)
+            already_in_list.extend(task_dict_list)
             await self.cache.set(user_id, already_in_list)
         else:
             await self.cache.set(user_id, task_dict_list)
@@ -21,7 +21,7 @@ class TaskProgress:
         ]
         if await self.cache.exists(f"{user_id}_already"):
             already_in_list = await self.cache.get(f"{user_id}_already")
-            already_in_list.append(already_dict_list)
+            already_in_list.extend(already_dict_list)
             await self.cache.set(f"{user_id}_already", already_in_list)
         else:
             await self.cache.set(f"{user_id}_already", already_dict_list)
