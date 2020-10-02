eb92b65a4d010b1ca9a5dfe56d5dc620276f47d2
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-02 18:35:27 +0900

    style(Heliotrope): Update 2.6.1

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "2.5.1"
+__version__ = "2.6.1"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=2, minor=5, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=2, minor=6, micro=1, releaselevel="final", serial=0)
