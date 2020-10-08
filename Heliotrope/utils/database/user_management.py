8b863050dee56f8996b9e4ad417bc0b841ed7d57
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-08 20:06:14 +0900

    refactor(database,download): functionalization

--- Heliotrope/utils/database/user_management.py
+++ Heliotrope/utils/database/user_management.py
@@ -16,3 +16,22 @@ async def user_register(user_id: int, check: bool = False):
         else:
             await User.create(user_id=user_id)
             return json({"status": 201, "message": "successfully"}, 201)
+
+async def user_download_count_check(user_id: int):
+    user_data = await User.get_or_none(user_id=user_id)  # 따로 나눠야함
+    if not user_data:
+        return json({"status": 403, "message": "need_register"}, 403)
+    else:
+        count = user_data.download_count
+        if count >= 5:
+            return json({"status": 429, "message": "Too_many_requests"}, 429)
+        else:
+            return True
+
+async def user_download_count(user_id: int):
+    user_data = await User.get_or_none(user_id=user_id)
+    count = user_data.download_count
+    user_data.download_count = count + 1
+    await user_data.save()
+    return 5 - user_data.download_count
+        
