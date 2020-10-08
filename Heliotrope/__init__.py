6057dee02ab8055fb00135fb51b0de10979f4baf
Restyled.io <commits@restyled.io>
2020-10-08 14:17:00 +0000

    style(black): apply code style

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -5,4 +5,3 @@ __version__ = "2.6.3"
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
 version_info = VersionInfo(major=2, minor=6, micro=3, releaselevel="final", serial=0)
-
