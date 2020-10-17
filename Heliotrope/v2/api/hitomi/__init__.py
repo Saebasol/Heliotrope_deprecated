4f70fb35e8fefd87f1468cee1f5fda650fce5af6
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-18 01:11:47 +0900

    feat(api/hitomi): add index route

--- Heliotrope/v2/api/hitomi/__init__.py
+++ Heliotrope/v2/api/hitomi/__init__.py
@@ -4,5 +4,6 @@ from .galleryinfo import galleryinfo
 from .info import info
 from .integrated import integrated
 from .list import list_
+from .index import index
 
-hitomi = Blueprint.group(info, galleryinfo, integrated, list_, url_prefix="/hitomi")
+hitomi = Blueprint.group(info, galleryinfo, integrated, list_, index, url_prefix="/hitomi")
