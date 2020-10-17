00bf714ca9069f9c8b8d166c9fc757696df1dc36
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-18 01:30:13 +0900

    fix(index): forget await

--- Heliotrope/v2/api/hitomi/index.py
+++ Heliotrope/v2/api/hitomi/index.py
@@ -10,5 +10,5 @@ index = Blueprint("hitomi_index_list", url_prefix="/index")
 @index.route("/")
 @authorized()
 async def hitomi_index(request):
-    index_list = hitomi.index()
+    index_list = await hitomi.index()
     return json(index_list)
