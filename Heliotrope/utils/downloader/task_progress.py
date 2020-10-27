65ad49afc2b98a92e1b331ab5e8605a4b33bdbcd
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-27 20:26:31 +0900

    refactor(task): now get user_id when init class

--- Heliotrope/utils/downloader/task_progress.py
+++ Heliotrope/utils/downloader/task_progress.py
@@ -4,25 +4,26 @@ import aiocache
 
 
 class TaskProgress:
-    def __init__(self) -> None:
+    def __init__(self, user_id) -> None:
+        self.user_id = user_id
         self.cache = aiocache.Cache()
 
-    async def cache_task(self, user_id, count: int, task: asyncio.Task):
+    async def cache_task(self, count: int, task: asyncio.Task):
         task_dict_list = [{"task_name": task.get_name(), "count": count, "task": task}]
-        if await self.cache.exists(user_id):
-            already_in_list = await self.cache.get(user_id)
+        if await self.cache.exists(self.user_id):
+            already_in_list = await self.cache.get(self.user_id)
             already_in_list.extend(task_dict_list)
-            await self.cache.set(user_id, already_in_list)
+            await self.cache.set(self.user_id, already_in_list)
         else:
-            await self.cache.set(user_id, task_dict_list)
+            await self.cache.set(self.user_id, task_dict_list)
 
-    async def cache_already(self, user_id, index, count, status, link):
+    async def cache_already(self, index, count, status, link):
         already_dict_list = [
             {"index": index, "count": count, "task_status": status, "link": link}
         ]
-        if await self.cache.exists(f"{user_id}_already"):
-            already_in_list = await self.cache.get(f"{user_id}_already")
+        if await self.cache.exists(f"{self.user_id}_already"):
+            already_in_list = await self.cache.get(f"{self.user_id}_already")
             already_in_list.extend(already_dict_list)
-            await self.cache.set(f"{user_id}_already", already_in_list)
+            await self.cache.set(f"{self.user_id}_already", already_in_list)
         else:
-            await self.cache.set(f"{user_id}_already", already_dict_list)
+            await self.cache.set(f"{self.user_id}_already", already_dict_list)
