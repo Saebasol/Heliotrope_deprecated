cf503f140ae010dbadf380d44f04bf27dfc5f617
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-27 20:28:35 +0900

    refactor(api/download): use class

--- Heliotrope/v2/api/download.py
+++ Heliotrope/v2/api/download.py
@@ -2,7 +2,7 @@ from sanic import Blueprint
 from sanic.response import json
 
 from Heliotrope.utils.checker.check import authorized
-from Heliotrope.utils.downloader.download import check_folder_and_download
+from Heliotrope.utils.downloader.download import Download
 
 download = Blueprint("image_download", url_prefix="/download")
 
@@ -16,11 +16,8 @@ async def api_download(request):
     index = request.json.get("index")
     user_id = request.json.get("user_id")
     download_bool = request.json.get("download")
-    if download_bool is False:
-        result = await check_folder_and_download(index, download_bool)
-    elif download_bool is None or not index or not user_id:
+    if download_bool is None or not index or not user_id:
         return json({"status": 400, "message": "bad_request"}, 400)
     else:
-        result = await check_folder_and_download(index, download_bool, user_id)
-
-    return result
+        download = Download(index, download_bool, user_id)
+        return await download.download()
