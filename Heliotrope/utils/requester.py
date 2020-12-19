205e6467aec78c576a5e4ed847e2cff7451a1c98
Ryu JuHeon <solo@doujinshiman.ga>
2020-12-20 02:52:06 +0900

    feat(utils/requester): add Request class

---
+++ Heliotrope/utils/requester.py
@@ -0,0 +1,59 @@
+import aiohttp
+from aiohttp.client_reqrep import ClientResponse
+
+
+class Response:
+    def __init__(self, status, message, body):
+        self.status = status
+        self.message = message
+        self.body = body
+
+
+class Request:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    @property
+    def headers(self):
+        return self.kwargs.get("headers")
+
+    async def fetch(
+        self,
+        session: aiohttp.ClientSession,
+        url: str,
+        method: str,
+        response_method: str,
+        *args,
+        **kwargs,
+    ) -> ClientResponse:
+        async with session.request(method, url, *args, **kwargs) as response:
+            dispatch = {
+                "json": response.json,
+                "read": response.read,
+                "text": response.text,
+            }
+            if response_method not in dispatch:
+                raise ValueError(f"Invalid response_method value: {response_method}")
+            return Response(
+                response.status, response.reason, await dispatch[response_method]()
+            )
+
+    async def request(
+        self, url: str, method: str, response_method: str, *args, **kwargs
+    ) -> Response:
+        async with aiohttp.ClientSession(*self.args, **self.kwargs) as session:
+            response = await self.fetch(
+                session, url, method, response_method, *args, **kwargs
+            )
+            return response
+
+    async def get(
+        self, url: str, response_method: str = "read", *args, **kwargs
+    ) -> Response:
+        """Perform HTTP GET request."""
+        return await self.request(url, "GET", response_method, *args, **kwargs)
+
+    async def post(self, url: str, response_method: str, *args, **kwargs) -> Response:
+        """Perform HTTP POST request."""
+        return await self.request(url, "POST", response_method, *args, **kwargs)
