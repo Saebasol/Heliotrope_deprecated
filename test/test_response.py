2d30c9b5489f719b3b660f8308777610127b7f15
Ryu JuHeon <saidbysolo@gmail.com>
2020-12-20 15:45:21 +0900

    test(test): version v3

--- test/test_response.py
+++ test/test_response.py
@@ -5,25 +5,25 @@ headers = {"Authorization": os.environ["Authorization"]}
 
 
 async def test_info_response(test_cli):
-    response = await test_cli.get("/v2/api/hitomi/info/1496588", headers=headers)
+    response = await test_cli.get("/v3/api/hitomi/info/1496588", headers=headers)
     assert response.status == 200
 
 
 async def test_galleryindo_response(test_cli):
-    response = await test_cli.get("/v2/api/hitomi/galleryinfo/1496588", headers=headers)
+    response = await test_cli.get("/v3/api/hitomi/galleryinfo/1496588", headers=headers)
     assert response.status == 200
 
 
 async def test_integrated_info_response(test_cli):
-    response = await test_cli.get("/v2/api/hitomi/integrated/1496588", headers=headers)
+    response = await test_cli.get("/v3/api/hitomi/integrated/1496588", headers=headers)
     assert response.status == 200
 
 
 async def test_list_response(test_cli):
-    response = await test_cli.get("/v2/api/hitomi/list/1", headers=headers)
+    response = await test_cli.get("/v3/api/hitomi/list/1", headers=headers)
     assert response.status == 200
 
 
 async def test_index_response(test_cli):
-    response = await test_cli.get("/v2/api/hitomi/index", headers=headers)
+    response = await test_cli.get("/v3/api/hitomi/index", headers=headers)
     assert response.status == 200
