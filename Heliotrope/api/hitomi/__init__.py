6b0e7ede0aecfd090e4f5eda220ffd1f7b735e24
Ryu JuHeon <saidbysolo@gmail.com>
2020-12-20 16:19:47 +0900

    feat(hitomi): register images route

--- Heliotrope/api/hitomi/__init__.py
+++ Heliotrope/api/hitomi/__init__.py
@@ -5,7 +5,8 @@ from .index import index
 from .info import info
 from .integrated import integrated
 from .list import list_
+from .image_info import images
 
 hitomi = Blueprint.group(
-    info, galleryinfo, integrated, list_, index, url_prefix="/hitomi"
+    info, galleryinfo, integrated, list_, index, images, url_prefix="/hitomi"
 )
