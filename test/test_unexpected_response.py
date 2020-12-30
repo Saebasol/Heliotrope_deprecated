3172a4c826f316b949c8768957dffd9393fa74b9
Ryu JuHeon <saidbysolo@gmail.com>
2020-12-31 01:40:46 +0900

    test(test): add more test code

--- test/test_unexpected_response.py
+++ test/test_unexpected_response.py
@@ -1,6 +1,3 @@
-import os
-
-
 async def test_info_unexpected_response(test_cli):
     response = await test_cli.get("/v3/api/hitomi/info/1111111111111111")
     assert response.status == 404
@@ -13,4 +10,19 @@ async def test_galleryindo_unexpected_response(test_cli):
 
 async def test_list_unexpected_response(test_cli):
     response = await test_cli.get("/v3/api/hitomi/list/1111111111111111")
+    assert response.status == 404
+
+
+async def test_images_unexpected_response(test_cli):
+    response = await test_cli.get("/v3/api/hitomi/images/1111111111111111")
+    assert response.status == 200
+
+
+async def test_integrated_unexpected_response(test_cli):
+    response = await test_cli.get("/v3/api/hitomi/integrated/1111111111111111")
+    assert response.status == 200
+
+
+async def test_proxy_unexpected_reponse(test_cli):
+    response = await test_cli.get("/v3/api/proxy/this_is_test")
     assert response.status == 404
