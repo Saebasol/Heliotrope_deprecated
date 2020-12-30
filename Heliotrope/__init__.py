bdaf976caa44dcff6fce1f3bcba8b327beebf970
Ryu JuHeon <solo@doujinshiman.ga>
2020-12-31 01:56:45 +0900

    style(Heliotrope): Update 3.1.2
    on-behalf-of: @Saebasol <solo@doujinshiman.ga>

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "3.1.1"
+__version__ = "3.1.2"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=3, minor=1, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=3, minor=1, micro=2, releaselevel="final", serial=0)
