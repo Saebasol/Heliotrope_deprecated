c95bd548f3c5b77fac67790f8b68f36ece56d48a
SaidBySolo <saidbysolo@gmail.com>
2020-12-30 14:03:49 +0900

    test(test): not need authorization

--- test/test_unexpected_response.py
+++ test/test_unexpected_response.py
@@ -1,24 +1,16 @@
 import os
 
-headers = {"Authorization": os.environ["Authorization"]}
-
 
 async def test_info_unexpected_response(test_cli):
-    response = await test_cli.get(
-        "/v3/api/hitomi/info/1111111111111111", headers=headers
-    )
+    response = await test_cli.get("/v3/api/hitomi/info/1111111111111111")
     assert response.status == 404
 
 
 async def test_galleryindo_unexpected_response(test_cli):
-    response = await test_cli.get(
-        "/v3/api/hitomi/galleryinfo/1111111111111111", headers=headers
-    )
+    response = await test_cli.get("/v3/api/hitomi/galleryinfo/1111111111111111")
     assert response.status == 404
 
 
 async def test_list_unexpected_response(test_cli):
-    response = await test_cli.get(
-        "/v3/api/hitomi/list/1111111111111111", headers=headers
-    )
+    response = await test_cli.get("/v3/api/hitomi/list/1111111111111111")
     assert response.status == 404
