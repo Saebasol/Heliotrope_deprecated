12af2c32567e542d5ee23456caef863358bb710c
Ryu JuHeon <saidbysolo@gmail.com>
2021-01-02 00:34:54 +0900

    feat(user_management): add check param

--- Heliotrope/utils/database/user_management.py
+++ Heliotrope/utils/database/user_management.py
@@ -3,8 +3,13 @@ from sanic.response import json
 from Heliotrope.utils.database import User
 
 
-async def user_register(user_id):
+async def user_register(user_id, check=False):
     user_data = await User.get_or_none(user_id=user_id)
+    if check:
+        if user_data:
+            return True
+        else:
+            return False
     if user_data:
         return json({"status": 200, "message": "already_register"}, 200)
     else:
