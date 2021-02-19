8983547bbd05617e57925fd09ba80c2bb5c81b19
Ryu JuHeon <saidbysolo@gmail.com>
2021-02-19 20:55:53 +0900

    style(Heliotrope): Bump 3.3.7 to 3.3.8

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "3.3.7"
+__version__ = "3.3.8"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=3, minor=3, micro=7, releaselevel="final", serial=0)
+version_info = VersionInfo(major=3, minor=3, micro=8, releaselevel="final", serial=0)
