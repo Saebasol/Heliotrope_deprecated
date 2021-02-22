b4a97a3fe25bbe45c7408e5559219440a74094a6
ombe <h3236516@gmail.com>
2021-02-23 02:56:01 +0900

    feat(ranking): add ranking route

--- Heliotrope/api/__init__.py
+++ Heliotrope/api/__init__.py
@@ -3,5 +3,6 @@ from sanic import Blueprint
 from .hitomi import hitomi
 from .image_proxy import proxy
 from .register import register
+from .ranking import ranking
 
-api = Blueprint.group(hitomi, proxy, register, url_prefix="/api")
+api = Blueprint.group(hitomi, proxy, register, ranking, url_prefix="/api")
--- Heliotrope/utils/database/models/__init__.py
+++ Heliotrope/utils/database/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
+from .ranking import Ranking
 
-__all__ = ["User"]
+__all__ = ["User", "Ranking"]
