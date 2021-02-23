fe40addca7d1c47634b566d6551ea13cce4c924c
Restyled.io <commits@restyled.io>
2021-02-23 02:28:43 +0000

    style(isort): apply code style

--- Heliotrope/api/__init__.py
+++ Heliotrope/api/__init__.py
@@ -2,7 +2,7 @@ from sanic import Blueprint
 
 from .hitomi import hitomi
 from .image_proxy import proxy
-from .register import register
 from .ranking import ranking
+from .register import register
 
 api = Blueprint.group(hitomi, proxy, register, ranking, url_prefix="/api")
