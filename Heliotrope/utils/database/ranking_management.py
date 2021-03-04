55cf3a49bf5623daa6b833debcf27166abba66be
ombe <h3236516@gmail.com>
2021-03-04 18:41:22 +0900

    feat(ranking_management): add month data

--- Heliotrope/utils/database/ranking_management.py
+++ Heliotrope/utils/database/ranking_management.py
@@ -1,4 +1,5 @@
 from sanic.response import json
+from datetime import datetime
 
 from Heliotrope.utils.database import Ranking
 
@@ -22,7 +23,7 @@ async def view_ranking(check=False):
     if check:
         rank_list = await Ranking.all().values("index", "count")
         sorted_ranking = sorted(rank_list, key=lambda info: info["count"], reverse=True)
-        ranking = {"count": len(sorted_ranking), "list": sorted_ranking}
+        ranking = {"count": len(sorted_ranking), "month": datetime.today().month, "list": sorted_ranking}
         return json(ranking, 200)
     else:
         return json({"status": 400, "message": "bad_request"}, 400)
