85d9e399d9f5420755afc951b7500e5dd2f8dc55
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-13 17:14:48 +0900

    style(Heliotrope): Bump up 3.5.0 to 3.5.1

--- Heliotrope/__init__.py
+++ Heliotrope/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "3.5.0"
+__version__ = "3.5.1"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=3, minor=5, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=3, minor=5, micro=1, releaselevel="final", serial=0)
