22e7b044215d395c15839487d85163a726596e31
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-15 20:27:51 +0900

    feat(requester): change to using semapore

--- Heliotrope/utils/requester.py
+++ Heliotrope/utils/requester.py
@@ -1,99 +1,53 @@
-from typing import Any, Awaitable, Callable, Coroutine, Literal, TypedDict, Union
-
-import aiohttp
 from multidict import CIMultiDictProxy
 from yarl import URL
-
-
-class Dispatch(TypedDict):
-    json: Callable[[], Awaitable[Any]]
-    read: Callable[[], Awaitable[bytes]]
-    text: Callable[[], Awaitable[str]]
+from heliotrope.utils.decorators import strict_literal
+from aiohttp import ClientSession
+from asyncio.locks import Semaphore
+from typing import Any, Literal
 
 
 class Response:
     def __init__(
         self,
         status: int,
-        message: str,
+        reason: str,
         body: Any,
         url: URL,
         headers: CIMultiDictProxy[str],
     ):
         self.status = status
-        self.message = message
+        self.reason = reason
         self.body = body
         self.url = url
         self.headers = headers
 
 
-class Request:
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+class RestrictedRequester:
+    def __init__(self, semapore: Semaphore) -> None:
+        self.semapore = semapore
 
-    @property
-    def headers(self):
-        return self.kwargs.get("headers")
-
-    async def fetch(
-        self,
-        session: aiohttp.ClientSession,
-        url: str,
-        method: str,
-        response_method: Literal["json", "read", "text"],
-        *args,
-        **kwargs,
-    ) -> Response:
-        async with session.request(method, url, *args, **kwargs) as response:
-            dispatch: Dispatch = {
-                "json": response.json,
-                "read": response.read,
-                "text": response.text,
-            }
-            if response_method not in dispatch:
-                raise ValueError(f"Invalid response_method value: {response_method}")
-            return Response(
-                response.status,
-                response.reason,
-                await dispatch[response_method](),
-                response.url,
-                response.headers,
-            )
-
+    @strict_literal("return_method")
     async def request(
         self,
-        url: str,
         method: str,
-        response_method: Literal["json", "read", "text"],
-        *args,
-        **kwargs,
-    ) -> Response:
-        async with aiohttp.ClientSession(*self.args, **self.kwargs) as session:
-            response = await self.fetch(
-                session, url, method, response_method, *args, **kwargs
-            )
-            return response
-
-    async def get(
-        self,
         url: str,
-        response_method: Literal["json", "read", "text"] = "read",
-        *args,
-        **kwargs,
-    ) -> Response:
-        """Perform HTTP GET request."""
-        return await self.request(url, "GET", response_method, *args, **kwargs)
+        return_method: Literal["read", "text", "json"],
+        **kwargs: Any
+    ):
+        async with ClientSession() as cs:
+            async with self.semapore, cs.request(method, url, **kwargs) as r:
+                return Response(
+                    r.status,
+                    r.reason,
+                    await getattr(r, return_method)(),
+                    r.url,
+                    r.headers,
+                )
 
-    async def post(
-        self,
-        url: str,
-        response_method: Literal["json", "read", "text"],
-        *args,
-        **kwargs,
-    ) -> Response:
-        """Perform HTTP POST request."""
-        return await self.request(url, "POST", response_method, *args, **kwargs)
+    @strict_literal("return_method")
+    async def get(self, url, return_method: Literal["read", "text", "json"]):
+        return await self.request("GET", url, return_method)
 
-
-request = Request()
+    @strict_literal("return_method")
+    async def post(self, url, return_method: Literal["read", "text", "json"]):
+        return await self.request("POST", url, return_method)
