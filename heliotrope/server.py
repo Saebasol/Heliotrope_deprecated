7ed6ec9b753daf09c475c2c4c42eb7134dcf617d
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-18 20:51:21 +0900

    refactor!(server): add listener and handle test enviroment

--- heliotrope/server.py
+++ heliotrope/server.py
@@ -1,17 +1,25 @@
-from heliotrope.utils.requester import RestrictedRequester
-from asyncio.locks import Semaphore
+import os
+
+from aiohttp.client import ClientSession
 from sanic import Sanic
+
+from heliotrope.utils.requester import HitomiRequester
 
 heliotrope = Sanic("heliotrope")
 
-
-heliotrope.config.concurrency_per_worker = 4
-heliotrope.config.FORWARDED_SECRET
-heliotrope.config.DB_URL = None
-heliotrope.config.SENTRY_DSN = None
+if not os.environ.get("TEST"):
+    heliotrope.config.FORWARDED_SECRET = os.environ["FORWARDED_SECRET"]
+    heliotrope.config.DB_URL = os.environ["DB_URL"]
+    heliotrope.config.SENTRY_DSN = os.environ["SENTRY_DSN"]
 
 
 @heliotrope.listener("before_server_start")
 def init(heliotrope, loop):
-    sem = Semaphore(heliotrope.config.concurrency_per_worker, loop=loop)
-    heliotrope.request = RestrictedRequester(sem)
+    hitomi_session = ClientSession(loop=loop)
+    heliotrope.hitomi_requester = HitomiRequester(hitomi_session)
+
+
+@heliotrope.listener("after_server_stop")
+def finish(heliotrope, loop):
+    loop.run_until_complete(heliotrope.hitomi_session.close())
+    loop.close()
