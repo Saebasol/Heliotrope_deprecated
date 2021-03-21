002988315c92e8e418bf592cbec6234d96eff59e
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-21 15:57:57 +0900

    feat(galleryinfo): add galleryinfo route

--- heliotrope/api/hitomi/galleryinfo.py
+++ heliotrope/api/hitomi/galleryinfo.py
@@ -1,13 +1,20 @@
+from heliotrope.database.query import get_galleryinfo
 from sanic import Blueprint
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
-hitomi_galleyinfo = Blueprint("hitomi_list", url_prefix="/galleyinfo")
+from heliotrope.utils.response import not_found
+from heliotrope.utils.typed import HeliotropeRequest
+
+hitomi_galleyinfo = Blueprint("hitomi_galleyinfo", url_prefix="/galleryinfo")
 
 
 class HitomiGalleryInfoView(HTTPMethodView):
-    async def get(self, request, index):
-        return json({"status": 200})
+    async def get(self, request: HeliotropeRequest, index: int):
+        if galleryinfo := await get_galleryinfo(index):
+            galleryinfo.update({"status": 200})
+            return json(galleryinfo)
+        return not_found
 
 
-hitomi_galleyinfo.add_route(HitomiGalleryInfoView.as_view(), "<index:int>")
+hitomi_galleyinfo.add_route(HitomiGalleryInfoView.as_view(), "/<index:int>")
