161cf0089c31617fe75bdf57f6fc74b59e7912e0
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-21 15:57:32 +0900

    feat(query): add galleryinfo query

--- heliotrope/database/query.py
+++ heliotrope/database/query.py
@@ -1,4 +1,15 @@
+from typing import Union
+from heliotrope.database.models.hitomi import File, GalleryInfo, Tag
 from heliotrope.database.models.requestcount import RequestCount
+
+
+def remove_id_and_index_id(tag_or_file_list):
+    response_dict_list = []
+    for value in tag_or_file_list:
+        del value["id"]
+        del value["index_id"]
+        response_dict_list.append(value)
+    return response_dict_list
 
 
 async def get_all_request_count():
@@ -11,10 +22,25 @@ async def get_all_request_count():
     return ranking
 
 
-async def add_request_count(index):
+async def add_request_count(index: int):
     if index_data := await RequestCount.get_or_none(index=index):
         index_data.count += 1
         await index_data.save()
     else:
         index_data = await RequestCount.create(index=index, count=1)
         await index_data.save()
+
+
+async def get_galleryinfo(index: int):
+    if galleryinfo := await GalleryInfo.get_or_none(id=index):
+        galleryinfo = {
+            "id": galleryinfo.id,
+            "language_localname": galleryinfo.language_localname,
+            "date": galleryinfo.date,
+            "files": remove_id_and_index_id(await galleryinfo.files.all().values()),
+            "tags": remove_id_and_index_id(await galleryinfo.tags.all().values()),
+            "japanese_title": galleryinfo.japanese_title,
+            "title": galleryinfo.title,
+            "type": galleryinfo.type,
+        }
+        return galleryinfo
