9207be8edffbd3fb890f8c59219dd5c0608015b3
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-22 17:34:49 +0900

    style(isort): apply code style

--- heliotrope/api/hitomi/galleryinfo.py
+++ heliotrope/api/hitomi/galleryinfo.py
@@ -1,8 +1,9 @@
-from heliotrope.database.query import get_galleryinfo
 from sanic import Blueprint
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
+from heliotrope.database.query import get_galleryinfo
+from heliotrope.utils.hitomi.models import HitomiGalleryInfoModel
 from heliotrope.utils.response import not_found
 from heliotrope.utils.typed import HeliotropeRequest
 
