949e00fcbf4ac8db9a9454d713d4e43f784ebf81
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-22 19:34:46 +0900

    fix(api/hitomi/list): index must start at 0

--- heliotrope/api/hitomi/list.py
+++ heliotrope/api/hitomi/list.py
@@ -5,24 +5,28 @@ from sanic.response import json
 from sanic.views import HTTPMethodView
 
 from heliotrope.utils.typed import HeliotropeRequest
+from heliotrope.utils.response import not_found
 
 hitomi_list = Blueprint("hitomi_list", url_prefix="/list")
 
 
 class HitomiListView(HTTPMethodView):
     async def get(self, request: HeliotropeRequest, index: int):
-        hitomi_index_list = await request.app.ctx.hitomi_requester.fetch_index(index)
-        split_index_list = [
+        hitomi_index_list = await request.app.ctx.hitomi_requester.fetch_index()
+        split_hitomi_index_list = [
             hitomi_index_list[i * 15 : (i + 1) * 15]
             for i in range((len(hitomi_index_list) + 15 - 1) // 15)
         ]
-        if len(split_index_list) < index + 1:
-            return
+
+        start_at_zero = index - 1
+
+        if len(split_hitomi_index_list) < start_at_zero + 1 or start_at_zero < 0:
+            return not_found
 
         info_list = await gather(
             *[
                 request.app.ctx.hitomi_requester.get_info_using_index(index)
-                for index in split_index_list[index]
+                for index in split_hitomi_index_list[index]
             ]
         )
 
