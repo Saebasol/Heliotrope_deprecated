8d8c7e3ca6a2fcead0ce24064ed5fb37e33cea40
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-23 20:47:27 +0900

    style(galleryinfo, serever, conftest): remove unused module

--- heliotrope/api/hitomi/galleryinfo.py
+++ heliotrope/api/hitomi/galleryinfo.py
@@ -3,7 +3,6 @@ from sanic.response import json
 from sanic.views import HTTPMethodView
 
 from heliotrope.database.query import get_galleryinfo
-from heliotrope.utils.hitomi.models import HitomiGalleryInfoModel
 from heliotrope.utils.response import not_found
 from heliotrope.utils.typed import HeliotropeRequest
 
