140589abdc54e7defcc19f582ef3df81d1d65d7c
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-25 22:02:50 +0900

    feat(api/about): add about endpoint

---
+++ heliotrope/api/about.py
@@ -0,0 +1,52 @@
+from sanic import Blueprint
+from sanic.response import html, json
+from sanic.views import HTTPMethodView
+
+from heliotrope.utils.typed import HeliotropeRequest
+
+heliotrope_about = Blueprint("heliotrope_about", url_prefix="/about")
+
+
+class HeliotropeAboutView(HTTPMethodView):
+    async def get(self, request: HeliotropeRequest):
+        manager = request.app.ctx.mirroring_manager
+        if is_json := request.args.get("json"):
+            if is_json in ["True", "true"]:
+                return json(
+                    {
+                        "last_checked_time": manager.last_checked_time,
+                        "mirroring_time": manager.mirroring_time,
+                        "new_item": manager.new_item,
+                        "server_status": manager.status,
+                    }
+                )
+
+        return html(
+            f"""
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Heliotrope About</title>
+    <meta charset="utf-8" />
+</head>
+
+<body style="text-align:center;">
+    <h1>About Heliotrope</h1>
+    <div>
+        <p>Last checked time: {manager.last_checked_time}</p>
+        <p>Mirroring time: {manager.mirroring_time}</p>
+        <p>New item: {manager.new_item}</p>
+        <p>Server status: {manager.status}</p>
+    </div>
+    <footer>
+        <i><a href="https://github.com/Saebasol/Heliotrope">Saebasol Heliotrope</i>
+    </footer>
+</body>
+
+</html>
+"""
+        )
+
+
+heliotrope_about.add_route(HeliotropeAboutView.as_view(), "")
