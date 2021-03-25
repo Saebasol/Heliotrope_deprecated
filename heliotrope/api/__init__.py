770e6ced11cfe651eea6747f5e3eb615b65c0c54
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-25 22:03:14 +0900

    feat(api/__init__): register about endpoint

--- heliotrope/api/__init__.py
+++ heliotrope/api/__init__.py
@@ -1,3 +1,4 @@
+from heliotrope.api.about import heliotrope_about
 from sanic.blueprints import Blueprint
 
 from heliotrope import version_info
@@ -5,5 +6,9 @@ from heliotrope.api.count import request_count
 from heliotrope.api.hitomi import hitomi_endpoint
 
 heliotrope_endpoint = Blueprint.group(
-    hitomi_endpoint, request_count, url_prefix="/api", version=f"v{version_info.major}"
+    hitomi_endpoint,
+    request_count,
+    heliotrope_about,
+    url_prefix="/api",
+    version=f"v{version_info.major}",
 )
