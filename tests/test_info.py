336fc8fca9f8dad92e2e7df68c66887c8b0f731f
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-28 12:42:14 +0900

    style(tests): explicitly func name

--- tests/test_info.py
+++ tests/test_info.py
@@ -3,6 +3,6 @@ def test_get_info_response(app):
     assert response.status == 200
 
 
-def test_get_info_unexpected_response(app):
+def test_get_info_not_found_response(app):
     _, response = app.test_client.get("/v4/api/hitomi/info/0")
     assert response.status == 404
--- tests/test_list.py
+++ tests/test_list.py
@@ -3,6 +3,6 @@ def test_get_list_response(app):
     assert response.status == 200
 
 
-def test_get_list_unexpected_response(app):
+def test_get_list_not_found_response(app):
     _, response = app.test_client.get("/v4/api/hitomi/list/0")
     assert response.status == 404
