3d56966c2af5916051d377aab62d647e31d813f0
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-28 12:34:55 +0900

    style(api/__init__): apply rename

--- heliotrope/routes/api/__init__.py
+++ heliotrope/routes/api/__init__.py
@@ -1,12 +1,14 @@
 from sanic.blueprints import Blueprint
 
 from heliotrope import version_info
-from heliotrope.routes.api.count import request_count
+from heliotrope.routes.api.count import heliotrope_request_count
 from heliotrope.routes.api.hitomi import hitomi_endpoint
+from heliotrope.routes.api.proxy import heliotrope_image_proxy
 
-heliotrope_api_endpoint = Blueprint.group(
+heliotrope_api = Blueprint.group(
     hitomi_endpoint,
-    request_count,
+    heliotrope_request_count,
+    heliotrope_image_proxy,
     url_prefix="/api",
     version=f"v{version_info.major}",
 )
