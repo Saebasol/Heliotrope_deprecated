a5716ef5fad1814b10fe15cb8e134797bc50e6c7
Ryu JuHeon <saidbysolo@gmail.com>
2021-03-28 12:35:43 +0900

    style(routes): apply rename var

--- heliotrope/routes/__init__.py
+++ heliotrope/routes/__init__.py
@@ -1,6 +1,6 @@
 from sanic.blueprints import Blueprint
 
 from heliotrope.routes.about import heliotrope_about
-from heliotrope.routes.api import heliotrope_api_endpoint
+from heliotrope.routes.api import heliotrope_api
 
-heliotrope_routes = Blueprint.group(heliotrope_api_endpoint, heliotrope_about)
+heliotrope_routes = Blueprint.group(heliotrope_api, heliotrope_about)
