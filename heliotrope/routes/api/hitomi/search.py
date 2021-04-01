dbb4f03b0bf85db7f480133379c71ef283a8ae14
Ryu JuHeon <saidbysolo@gmail.com>
2021-04-01 22:24:06 +0900

    feat(search): add search endpoint

---
+++ heliotrope/routes/api/hitomi/search.py
@@ -0,0 +1,21 @@
+from sanic import Blueprint
+from sanic.response import json
+from sanic.views import HTTPMethodView
+
+from heliotrope.database.query import search_galleryinfo
+from heliotrope.utils.response import not_found
+from heliotrope.utils.typed import HeliotropeRequest
+
+hitomi_search = Blueprint("hitomi_search", url_prefix="/search")
+
+
+class HitomiSearchView(HTTPMethodView):
+    async def get(self, request: HeliotropeRequest):
+        if query := request.args.get("q"):
+            if result := await search_galleryinfo(query):
+                return json({"status": 200, "result": result})
+
+        return not_found
+
+
+hitomi_search.add_route(HitomiSearchView.as_view(), "")
