641a90f765d2c27c49ef6c8f1de1e3c71ac5decc
Ryu JuHeon <saidbysolo@gmail.com>
2021-04-20 00:22:14 +0900

    fix(galleryinfo, search): add raw parameter

--- heliotrope/routes/api/hitomi/search.py
+++ heliotrope/routes/api/hitomi/search.py
@@ -1,3 +1,4 @@
+from heliotrope.utils.hitomi.models import HitomiGalleryInfoModel
 from sanic import Blueprint
 from sanic.response import json
 from sanic.views import HTTPMethodView
@@ -14,8 +15,35 @@ class HitomiSearchView(HTTPMethodView):
         if (query := request.args.get("q")) and (
             result := await search_galleryinfo(query)
         ):
-            return json({"status": 200, "result": result})
+            if (is_true := request.args.get("raw")) and (is_true.lower() == "true"):
+                return json({"status": 200, "result": result})
 
+            return json(
+                {
+                    "status": 200,
+                    "result": list(
+                        map(
+                            lambda parsed_galleryinfo_model: {
+                                "language_localname": parsed_galleryinfo_model.language_localname,
+                                "language": parsed_galleryinfo_model.language,
+                                "date": parsed_galleryinfo_model.date,
+                                "files": parsed_galleryinfo_model.files,
+                                "tags": parsed_galleryinfo_model.tags,
+                                "japanese_title": parsed_galleryinfo_model.japanese_title,
+                                "title": parsed_galleryinfo_model.title,
+                                "id": parsed_galleryinfo_model.galleryid,
+                                "type": parsed_galleryinfo_model.hitomi_type,
+                            },
+                            map(
+                                lambda raw_result: HitomiGalleryInfoModel.parse_galleryinfo(
+                                    raw_result, True
+                                ),
+                                result,
+                            ),
+                        )
+                    ),
+                }
+            )
         return not_found
 
 
