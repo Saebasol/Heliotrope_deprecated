18cd82db33fc6c7b73225966e7b8c2cd3286f37c
Ryu JuHeon <saidbysolo@gmail.com>
2021-04-22 00:25:13 +0900

    test(test): add more test case

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,6 +3,11 @@ def test_get_search_response(app):
     assert response.status == 200
 
 
+def test_get_search_raw_response(app):
+    _, response = app.test_client.get("/v4/api/hitomi/search?q=sekigahara&raw=true")
+    assert response.status == 200
+
+
 def test_get_search_not_found_response(app):
     _, response = app.test_client.get("/v4/api/hitomi/search?q=testqueryxd")
     assert response.status == 404
