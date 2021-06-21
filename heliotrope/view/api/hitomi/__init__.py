1dc9d4a0704afd56516a353bbc1eae113f44e308
Ryu JuHeon <saidbysolo@gmail.com>
2021-06-21 18:55:25 +0900

    refactor(routes): rename folder

---
+++ heliotrope/view/api/hitomi/__init__.py
@@ -0,0 +1,16 @@
+from sanic.blueprints import Blueprint
+
+from heliotrope.view.api.hitomi.galleryinfo import hitomi_galleyinfo
+from heliotrope.view.api.hitomi.images import hitomi_images
+from heliotrope.view.api.hitomi.info import hitomi_info
+from heliotrope.view.api.hitomi.list import hitomi_list
+from heliotrope.view.api.hitomi.search import hitomi_search
+
+hitomi_endpoint = Blueprint.group(
+    hitomi_galleyinfo,
+    hitomi_images,
+    hitomi_info,
+    hitomi_list,
+    hitomi_search,
+    url_prefix="/hitomi",
+)
