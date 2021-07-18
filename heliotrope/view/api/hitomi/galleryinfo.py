60376783546ea4a6bbeb25d8bb4da3b24cc658bd
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-19 02:08:16 +0900

    style(view/.../galleryinfo): add HTTPResponse type hint

--- heliotrope/view/api/hitomi/galleryinfo.py
+++ heliotrope/view/api/hitomi/galleryinfo.py
@@ -2,13 +2,14 @@ from heliotrope.server import HeliotropeRequest
 
 from sanic.blueprints import Blueprint
 from sanic.views import HTTPMethodView
+from sanic.response import HTTPResponse
 
 hitomi_galleryinfo = Blueprint("hitomi_galleryinfo", url_prefix="/galleryinfo")
 
 
 class HitomiGalleryInfoView(HTTPMethodView):
-    async def get(self, request: HeliotropeRequest):
-        pass
+    async def get(self, request: HeliotropeRequest) -> HTTPResponse:
+        return request.app.ctx.response.not_found
 
 
 # TODO: add_route is partially unknown and as_view is partially unknown Need PR Sanic
