d6cb65dd85c8ec93724c1eaa05b581da061eb314
Ryu JuHeon <solo@doujinshiman.ga>
2021-07-19 10:54:15 +0900

    feat(view/...): add groups

---
+++ heliotrope/view/api/hitomi/__init__.py
@@ -0,0 +1,6 @@
+from sanic.blueprints import Blueprint
+
+from heliotrope.view.api.hitomi.galleryinfo import hitomi_galleryinfo
+
+# NOTE: Will fixed
+hitomi_endpoint = Blueprint.group(hitomi_galleryinfo, url_prefix="/hitomi")  # type: ignore
