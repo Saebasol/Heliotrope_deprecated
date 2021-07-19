5a7e1db2e46778d32f7ca0cc4777b16bb957c263
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-19 10:54:15 +0900

    feat(view/...): add groups

---
+++ heliotrope/view/api/__init__.py
@@ -0,0 +1,7 @@
+from sanic.blueprints import Blueprint
+
+from heliotrope import version_info
+from heliotrope.view.api.hitomi import hitomi_endpoint
+
+# NOTE: Will fixed
+api_endpoint = Blueprint.group(hitomi_endpoint, url_prefix="/api", version=version_info.major)  # type: ignore
