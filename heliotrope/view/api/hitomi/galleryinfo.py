9edeb55f5cdc06fcd87a0bcaa68b64aceeaed5c1
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-19 10:53:36 +0900

    style(view/.../galleryinfo): rename class name and add index arg

--- heliotrope/view/api/hitomi/galleryinfo.py
+++ heliotrope/view/api/hitomi/galleryinfo.py
@@ -1,16 +1,16 @@
-from heliotrope.server import HeliotropeRequest
-
 from sanic.blueprints import Blueprint
-from sanic.views import HTTPMethodView
 from sanic.response import HTTPResponse
+from sanic.views import HTTPMethodView
+
+from heliotrope.sanic import HeliotropeRequest
 
 hitomi_galleryinfo = Blueprint("hitomi_galleryinfo", url_prefix="/galleryinfo")
 
 
-class HitomiGalleryInfoView(HTTPMethodView):
-    async def get(self, request: HeliotropeRequest) -> HTTPResponse:
+class HitomiGalleryinfoView(HTTPMethodView):
+    async def get(self, request: HeliotropeRequest, index: int) -> HTTPResponse:
         return request.app.ctx.response.not_found
 
 
 # TODO: add_route is partially unknown and as_view is partially unknown Need PR Sanic
-hitomi_galleryinfo.add_route(HitomiGalleryInfoView.as_view(), "/<index:int>")  # type: ignore
+hitomi_galleryinfo.add_route(HitomiGalleryinfoView.as_view(), "/<index:int>")  # type: ignore
