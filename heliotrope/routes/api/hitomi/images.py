cbf3d1d244a8cd50b39b066339eb90446e2499bc
Restyled.io <commits@restyled.io>
2021-07-27 04:21:57 +0000

    style(black): apply code style

--- heliotrope/routes/api/hitomi/images.py
+++ heliotrope/routes/api/hitomi/images.py
@@ -35,7 +35,7 @@ class HitomiImagesInfoView(HTTPMethodView):
                         },
                         files,
                     )
-                )
+                ),
             }
         )
 
