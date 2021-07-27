1d1a86d26ace49bcb3e9387aa839f66dc10ff3ca
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-27 13:21:28 +0900

    style(heliotrope): Bump up 4.0.1 to 4.0.2

--- heliotrope/__init__.py
+++ heliotrope/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "4.0.1"
+__version__ = "4.0.2"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=4, minor=0, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=4, minor=0, micro=2, releaselevel="final", serial=0)
