07c33c80a2bbfbdf8f80d13561563c81c9f18065
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-03 01:29:27 +0900

    feat(view/.../__init__): add endpoint

--- heliotrope/view/api/__init__.py
+++ heliotrope/view/api/__init__.py
@@ -2,8 +2,9 @@ from sanic.blueprints import Blueprint
 
 from heliotrope import version_info
 from heliotrope.view.api.hitomi import hitomi_endpoint
+from heliotrope.view.api.proxy import proxy
 
 # NOTE: Will fixed
 api_endpoint = Blueprint.group(
-    hitomi_endpoint, url_prefix="/api", version=version_info.major
+    hitomi_endpoint, proxy, url_prefix="/api", version=version_info.major
 )
--- heliotrope/view/api/hitomi/__init__.py
+++ heliotrope/view/api/hitomi/__init__.py
@@ -1,6 +1,9 @@
 from sanic.blueprints import Blueprint
 
 from heliotrope.view.api.hitomi.galleryinfo import hitomi_galleryinfo
+from heliotrope.view.api.hitomi.images import hitomi_images
 
 # NOTE: Will fixed
-hitomi_endpoint = Blueprint.group(hitomi_galleryinfo, url_prefix="/hitomi")
+hitomi_endpoint = Blueprint.group(
+    hitomi_galleryinfo, hitomi_images, url_prefix="/hitomi"
+)
