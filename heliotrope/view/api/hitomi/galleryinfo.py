9466d1046fcf27585698b3813ba560b8a3b6d602
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-02 15:47:24 +0900

    feat(galleryinfo_: implement galleyinfo endpoint

--- heliotrope/view/api/hitomi/galleryinfo.py
+++ heliotrope/view/api/hitomi/galleryinfo.py
@@ -1,5 +1,5 @@
 from sanic.blueprints import Blueprint
-from sanic.response import HTTPResponse
+from sanic.response import HTTPResponse, json
 from sanic.views import HTTPMethodView
 
 from heliotrope.sanic import HeliotropeRequest
@@ -8,9 +8,12 @@ hitomi_galleryinfo = Blueprint("hitomi_galleryinfo", url_prefix="/galleryinfo")
 
 
 class HitomiGalleryinfoView(HTTPMethodView):
-    async def get(self, request: HeliotropeRequest, index: int) -> HTTPResponse:
+    async def get(self, request: HeliotropeRequest, index_id: int) -> HTTPResponse:
+        if galleryinfo := request.app.ctx.orm_query.get_galleryinfo(index_id):
+            return json(galleryinfo)
+
         return request.app.ctx.response.not_found
 
 
 # TODO: add_route is partially unknown and as_view is partially unknown Need PR Sanic
-hitomi_galleryinfo.add_route(HitomiGalleryinfoView.as_view(), "/<index:int>")  # type: ignore
+hitomi_galleryinfo.add_route(HitomiGalleryinfoView.as_view(), "/<index_id:int>")  # type: ignore
