8e401e2131b59a581eab11094226e967fa0af612
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-03 01:30:42 +0900

    style(sanic): add type hint

--- heliotrope/sanic.py
+++ heliotrope/sanic.py
@@ -1,15 +1,20 @@
 from types import SimpleNamespace
 
 from sanic.app import Sanic
-from sanic.request import Request
 from sanic.config import Config
+from sanic.request import Request
+
 from heliotrope.database.query import ORMQuery
+from heliotrope.request.base import BaseRequest
+from heliotrope.request.hitomi import HitomiRequest
 from heliotrope.response import Response
 
 
 class HeliotropeContext(SimpleNamespace):
     orm_query: ORMQuery
     response: Response
+    hitomi_request: HitomiRequest
+    base_request: BaseRequest
 
 
 class Heliotrope(Sanic):
