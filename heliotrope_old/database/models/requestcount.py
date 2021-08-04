256fda575dd7f7d70b263bc48aefc517e3817877
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-04 15:35:31 +0900

    chore!(heliotrope_old): remove old file

--- heliotrope_old/database/models/requestcount.py
+++
@@ -1,7 +0,0 @@
-from tortoise import Model, fields
-
-
-class RequestCount(Model):
-    index = fields.IntField(pk=True)
-    title = fields.CharField(255)
-    count: int = fields.IntField()
--- heliotrope_old/view/__init__.py
+++
@@ -1,6 +0,0 @@
-from sanic.blueprints import Blueprint
-
-from heliotrope.view.about import heliotrope_about
-from heliotrope.view.api import heliotrope_api
-
-heliotrope_view = Blueprint.group(heliotrope_api, heliotrope_about)
